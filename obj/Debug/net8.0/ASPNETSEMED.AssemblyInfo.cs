3ff00c683a6345855ef1fd01b81b71cbc490f9e6
Antônio Eduardo Souza de Oliveira <antoniooliveira@semed.maceio.al.gov.br>
2024-06-14 10:41:02 -0300

    Continuando cadastramento impressora

--- obj/Debug/net8.0/ASPNETSEMED.AssemblyInfo.cs
+++ obj/Debug/net8.0/ASPNETSEMED.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASPNETSEMED")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e5f69213433f5a21fb921db3619a740c655b762")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b1fe614a38f501b04ce3c5e97cd1b956f644007")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASPNETSEMED")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASPNETSEMED")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
